94e5371d4f189639489d267eaaa3a93a81309534
said13 <abdullah.atkaev@gmail.com>
2024-12-08 19:33:51 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 import type { NextConfig } from "next";
 
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
-const repoName = process.env.REPO_NAME || '<repository-name>'; // Replace <repository-name> with your repo name if not using .env
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export',
-  trailingSlash: true,
-  assetPrefix: isGitHubPages ? `/${repoName}/` : undefined,
-  basePath: isGitHubPages ? `/${repoName}` : undefined,
+  basePath: '/tma-app'
 };
 
 export default nextConfig;
