db89dc1434d27f1c6661f961e65a526106618088
said13 <abdullah.atkaev@gmail.com>
2024-12-08 20:13:37 +0300

    Update global.d.ts

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,17 +1,22 @@
 interface WebAppType {
     initDataUnsafe: {
-      user: {
-        id: number;
-        first_name: string;
-        last_name?: string;
-        username?: string;
-        language_code?: string;
-        is_premium?: boolean;
-      };
+        user: {
+            id: number;
+            first_name: string;
+            last_name?: string;
+            username?: string;
+            language_code?: string;
+            is_premium?: boolean;
+        };
     };
-  }
-  
-  interface Window {
+}
+
+interface Telegram {
     WebApp?: WebAppType;
-  }
-  
+}
+
+declare global {
+    interface Window {
+        Telegram: any;
+    }
+}
