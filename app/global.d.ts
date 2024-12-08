230314ec1577d36b93c5990b1734d096cfc111be
said13 <abdullah.atkaev@gmail.com>
2024-12-08 19:46:39 +0300

    add global file

---
+++ app/global.d.ts
@@ -0,0 +1,17 @@
+interface WebAppType {
+    initDataUnsafe: {
+      user: {
+        id: number;
+        first_name: string;
+        last_name?: string;
+        username?: string;
+        language_code?: string;
+        is_premium?: boolean;
+      };
+    };
+  }
+  
+  interface Window {
+    WebApp?: WebAppType;
+  }
+  
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-'use client'
+'use client';
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-interface UserData { 
+interface UserData {
   id: number;
   first_name: string;
   last_name?: string;
@@ -10,36 +10,37 @@ interface UserData {
   language_code?: string;
   is_premium?: boolean;
 }
+
 export default function Home() {
   const [userData, setUserData] = useState<UserData | null>(null);
 
   useEffect(() => {
-    if (typeof window !== 'undefined' && window.WebApp && window.WebApp.initDataUnsafe.user) {
-      setUserData(window.WebApp.initDataUnsafe.user as UserData);
+    if (
+      typeof window !== 'undefined' &&
+      window.WebApp &&
+      window.WebApp.initDataUnsafe?.user
+    ) {
+      setUserData(window.WebApp.initDataUnsafe.user);
     }
   }, []);
-  
+
   return (
     <main className="p-4">
-      {
-        userData ? 
-        (
-          <>
-            <h1 className="text-2xl font-bold mb-4">User Data</h1>
-            <ul>
-              <li>ID: {userData.id}</li>
-              <li>First Name: {userData.first_name}</li>
-              <li>Last Name: {userData.last_name}</li>
-              <li>Username: {userData.username}</li>
-              <li>Language Code: {userData.language_code}</li>
-              <li>Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
-            </ul>
-          </>
-        ) :
-        (
-          <div>Loading...</div>
-        )
-      }
+      {userData ? (
+        <>
+          <h1 className="text-2xl font-bold mb-4">User Data</h1>
+          <ul>
+            <li>ID: {userData.id}</li>
+            <li>First Name: {userData.first_name}</li>
+            <li>Last Name: {userData.last_name || 'N/A'}</li>
+            <li>Username: {userData.username || 'N/A'}</li>
+            <li>Language Code: {userData.language_code || 'N/A'}</li>
+            <li>Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
+          </ul>
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
     </main>
   );
 }
