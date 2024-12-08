fcc8646a4e2b347ed170b647b4e8a0c5e36f2793
said13 <abdullah.atkaev@gmail.com>
2024-12-08 19:24:48 +0300

    github deploy changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+const repoName = process.env.REPO_NAME || '<repository-name>'; // Replace <repository-name> with your repo name if not using .env
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: 'export'
+  output: 'export',
+  trailingSlash: true,
+  assetPrefix: isGitHubPages ? `/${repoName}/` : undefined,
+  basePath: isGitHubPages ? `/${repoName}` : undefined,
 };
 
 export default nextConfig;
